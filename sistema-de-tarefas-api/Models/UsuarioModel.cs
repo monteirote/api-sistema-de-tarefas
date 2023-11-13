304208945eafff3898ea2a303298d0eda4f102ae
Vinicius <vinicius@iconax.com.br>
2023-11-13 18:22:00 -0300

    adicionando models e UsuarioController

---
+++ sistema-de-tarefas-api/Models/UsuarioModel.cs
@@ -0,0 +1,11 @@
+namespace sistema_de_tarefas_api.Models;
+
+public class UsuarioModel
+{
+    public int Id { get; set; }
+    
+    public string? Nome { get; set; }
+    
+    public string? Email { get; set; }
+    
+}
