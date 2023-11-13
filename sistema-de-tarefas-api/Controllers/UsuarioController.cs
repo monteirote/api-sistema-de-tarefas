e5e100deaa6bad213d18cf058f9b115b3af8295b
Vinicius <vinicius@iconax.com.br>
2023-11-13 18:23:43 -0300

    adicionando controller

---
+++ sistema-de-tarefas-api/Controllers/UsuarioController.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using sistema_de_tarefas_api.Models;
+
+namespace sistema_de_tarefas_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<UsuarioModel>> BuscarTodosUsuarios()
+        {
+            return Ok();
+        }
+    }
+}
+  
